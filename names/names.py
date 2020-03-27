52020b0a7b7243954401cd64f684c9ff500c74f1
ngaivoronski <ngaivoronski@gmail.com>
2020-03-27 13:24:55 -0400

    names stretch complete

--- names/names.py
+++ names/names.py
@@ -57,9 +57,47 @@ for name in names_2:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"original runtime using data structure: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# create a new set to remove internal duplicates
+newSet = {"asdf"}
+
+# create a new duplicates array
+dupes2 = []
+
+# fill out the set with the values from names_1
+for name in names_1:
+    newSet.add(name)
+
+# use built-in sort function to sort the new set
+newSet2 = sorted(newSet)
+
+# binary search function
+def BinarySearch(lys, val):
+    first = 0
+    last = len(lys)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
+
+# run binary search on each name in names_2
+for name in names_2:
+    if BinarySearch(newSet2, name) >= 0:
+        dupes2.append(name)
+
+# print endtime
+end_time2 = time.time()
+print (f"runtime using built-in sorted function: {end_time2 - end_time} seconds")
